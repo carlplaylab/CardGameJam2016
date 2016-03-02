b280615ee3b4179173cc78c8aa51f35ab3f83b37
carlplaylab <carl.j@pocketplaylab.com>
2016-03-02 21:51:06 +0800

    Added more code for UI

---
+++ CardGame/Assets/Game/Scripts/UI/CounterDisplay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class CounterDisplay : MonoBehaviour 
+{
+	[SerializeField] private NumberIncrementText text;
+	[SerializeField] private RectTransform icon;
+
+	private int amount;
+
+
+	public int Amount
+	{
+		get { return amount; }
+		set {
+			amount = value; 
+			text.onStartAction = Focus;
+			text.Amount = amount;
+			text.onEndAction = UnFocus;
+		}
+	}
+
+	public void Focus ()
+	{
+		icon.transform.localScale = new Vector3(1.25f, 1.25f, 1f);
+	}
+
+	public void UnFocus ()
+	{
+		icon.transform.localScale = Vector3.one;
+	}
+
+}
